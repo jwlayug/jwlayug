6ca2b4eff70578fbe1b3b8100ee2e6f41acd3c67
jwlayug <jwlayug@gmail.com>
2014-01-07 11:54:55 +0800

    added jwlayug_template

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>JWLAYUG Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="_/css/bootstrap.css" rel="stylesheet">
+    <link href="_/css/main.css" rel="stylesheet">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+
+    <script src="_/js/bootstrap.js"></script>
+    <script src="_/js/main.js"></script>
+  </body>
+</html>
